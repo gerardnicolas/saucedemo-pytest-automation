61c06e624fcfd6c6a4526ce8963b0c57acf57533
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2025-09-21 00:21:54 +0800

    refactor: use XPATH for cart_item, revise WebDriverWait, add WebDriverWait

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -101,12 +101,18 @@ def test_remove_product_from_cart(browser):
     inventory_page.add_product_to_cart("sauce-labs-backpack")
     inventory_page.open_cart()
 
-    cart_page = CartPage(browser)
     print("Current URL:", browser.current_url)
-    WebDriverWait(browser, 5).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, "cart_item"))
-    )
-    assert cart_page.has_item("Sauce Labs Backpack")
-    cart_page.remove_item_in_cart("sauce-labs-backpack")
+    cart_item = (By.XPATH, "//div[@class='inventory_item_name' and text()='Sauce Labs Backpack']")
 
-    assert not cart_page.has_item("Sauce Labs Backpack"), f"Item was not removed from the cart"
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located(cart_item)
+    )
+
+    browser.find_element(By.ID, "remove-sauce-labs-backpack").click()
+
+    WebDriverWait(browser, 10).until(
+        EC.invisibility_of_element_located(cart_item)
+    )
+
+    items = browser.find_elements(By.CLASS_NAME, "inventory_item_name")
+    assert len(items) == 0, "Item was not removed from the cart"
