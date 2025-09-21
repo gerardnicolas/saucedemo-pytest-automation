30919bcabdb35ff1eb52efc65135835314c44671
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2025-09-22 04:33:34 +0800

    refactor: add explicit waits

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 class CartPage:
@@ -23,5 +24,16 @@ class CartPage:
         continue_shopping_btn = (By.ID, "continue-shopping")
         self.driver.find_element(*continue_shopping_btn).click()
 
-    def click_checkout(self):
-        self.driver.find_element(*self.checkout_btn).click()
+    def click_checkout(self, timeout: int = 20):
+        """Wait until checkout button is clickable, then click it."""
+        try:
+            checkout_element = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located(self.checkout_btn)
+            )
+            checkout_element.click()
+
+            WebDriverWait(self.driver, timeout).until(
+                EC.url_contains("/checkout-step-one.html")
+            )
+        except TimeoutException:
+            raise AssertionError("Checkout button was not present within the given time.")
