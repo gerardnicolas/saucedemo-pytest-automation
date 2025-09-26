69fcd7e1a9c681aeed7a37ce5fe205c825e1377c
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2025-09-26 16:52:42 +0800

    refactor: add helper function to check if locator is displayed

--- pages/checkout_page1.py
+++ pages/checkout_page1.py
@@ -1,3 +1,4 @@
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,14 +12,14 @@ class CheckoutPageOne:
         self.lastname_input = (By.ID, "last-name")
         self.postalcode_input = (By.ID, "postal-code")
 
-    def fill_input_field(self, locator, value, timeout=10):
-        element = WebDriverWait(self.driver, timeout).until(
-            EC.visibility_of_element_located(locator)
-        )
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
-        element.clear()
-        element.send_keys(str(value))
-        return element
+    # def fill_input_field(self, locator, value, timeout=20):
+    #     element = WebDriverWait(self.driver, timeout).until(
+    #         EC.visibility_of_element_located(locator)
+    #     )
+    #     self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    #     element.clear()
+    #     element.send_keys(str(value))
+    #     return element
 
     def _click_continue(self):
         self.driver.find_element(*self.continue_btn).click()
@@ -26,7 +27,17 @@ class CheckoutPageOne:
     def _verify_url(self):
         try:
             WebDriverWait(self.driver, 20).until(
-                EC.url_to_be("checkout-step-one.html")
+                EC.url_contains("checkout-step-one.html")
             )
-        except:
+
+        except TimeoutException:
             f"Did not reach /checkout-step-one.html page."
+
+    def is_displayed(self, locator, timeout=10):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                EC.visibility_of_element_located(locator)
+            )
+            return element.is_displayed()
+        except:
+            return False
