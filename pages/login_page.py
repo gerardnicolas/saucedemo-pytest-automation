1fa6a56706dd3dd776510308faa569576e92ce4b
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2025-10-21 02:22:11 +0800

    refactor login page to use basepage helper functions

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from .base_page import BasePage
+from pages.base_page import BasePage
 
 class LoginPage(BasePage):
     username_input = (By.ID, "user-name")
